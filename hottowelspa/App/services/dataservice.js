6cd3a6cec7fd7c4f2c47c0240fbd4af6607a107e
guy <coleman.guyg@gmail.com>
2013-11-10 16:59:10 +0200

    added static routing through a route class

--- hottowelspa/App/services/dataservice.js
+++ hottowelspa/App/services/dataservice.js
@@ -1,22 +1,44 @@
 ﻿define(['services/logger'],
     function (logger) {
+
         var dataservice = {
             getContactInfo: getContactInfo            
         };
 
         return dataservice;      
         
+        //Returns contact information
         function getContactInfo(callback) {
-            get(null, "HotTowel/ContactInfo", callback);
-                      
+            get(null, window.appRoutes.GeneralPaths.pathToContactInformation, callback);                      
         }
 
+
+
+
+
+
+
+        //#region Server calls
+
+        //Get requests
         function get(params, path, callback) {
             $.ajax({
                 url: path,
                 data: params,
                 type: "GET"
             }).done(callback); 
-        }        
+        }
+
+
+        //Post requests
+        function post(params, path, callback) {
+            $.ajax({
+                url: path,
+                data: params,
+                type: "POST"
+            }).done(callback);
+        }
+
+        //#endregion
 
 });
---
+++ hottowelspa/Utils/StaticServerPaths.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc; 
+
+namespace hottowelspa.Utils
+{
+    public static class StaticServerPaths
+    {
+        #region General
+
+        /// <summary>
+        /// Path to the Contact Info controller action
+        /// </summary> 
+        public static string PathToContactInformation(this UrlHelper url)
+        {
+            return url.Action("ContactInfo", new { Controller = "HotTowel" });
+        }
+
+        #endregion
+    }
+
+
+    public class StaticRoutes
+    {
+        
+
+        #region Constructor
+        public static object LoadStaticRoutes(UrlHelper url)
+        {
+            return new
+            {
+                GeneralPaths = SetupGeneralRoutes(url)
+            };
+        }
+
+        #endregion
+
+        #region Methods
+
+        private static object SetupGeneralRoutes(UrlHelper url)
+        {
+            return new
+            {
+                pathToContactInformation = url.PathToContactInformation()
+            }; 
+        }
+
+        #endregion
+
+    }
+}
